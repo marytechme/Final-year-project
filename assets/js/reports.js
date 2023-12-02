a8d76e292a284f86c2da53c7ca21c62016d6aedb
Mariam Tajudeen Sulaiman <marytechtajudeen@gmail.com>
2023-12-02 08:47:45 +0100

    commited all the files

---
+++ assets/js/reports.js
@@ -0,0 +1,32 @@
+const title = document.getElementById("title");
+const body = document.getElementById("body");
+const gender = document.getElementById("gender");
+const reportDate = document.getElementById("reportDate");
+
+
+document.getElementById("addReportForm").addEventListener("submit", e=>{
+    e.preventDefault();
+    let newData = {
+        title: title.value,
+        body: body.value,
+        date: reportDate.value,
+    }
+
+    const storedDataJSON = localStorage.getItem('reports');
+    if (storedDataJSON !== null) {
+        // The item exists in localStorage
+        let tableData = JSON.parse(storedDataJSON);
+        tableData.push(newData);
+        localStorage.setItem("reports", JSON.stringify(tableData));
+        getData()
+        //clear fields
+        title.value = ""
+        body.value = ""
+        reportDate.value = ""
+       
+      } 
+
+})
+
+
+
